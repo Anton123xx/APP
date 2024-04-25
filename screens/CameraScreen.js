280688d49f86ea563fcbca4a26060a49a1aaf08b
Anton123xx <115159228+Anton123xx@users.noreply.github.com>
2024-04-24 22:00:29 -0400

    push camera+

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,8 +1,9 @@
 import { Camera, CameraType } from 'expo-camera';
 import { useState } from 'react';
 import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React, {useRef } from 'react';
+import React, { useRef } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { MaterialIcons } from '@expo/vector-icons';
 
 //https://docs.expo.dev/versions/latest/sdk/camera/
 //faut implementer zoom et auto focus
@@ -18,20 +19,20 @@ export default function App({ navigation }) {
     return <View />;
   }
 
-  if (!permission.granted) { 
+  if (!permission.granted) {
     // Camera permissions are not granted yet
     return (
       <View style={styles.container}>
         <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+        <TouchableOpacity onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
   const setUserData = async (key, value) => {
-    try{
+    try {
       await AsyncStorage.setItem(key, value);
     }
-    catch(error){
+    catch (error) {
       console.error('Error storing data: ', error);
     }
   }
@@ -85,14 +86,23 @@ export default function App({ navigation }) {
             justifyContent: 'space-between',
             margin: 20,
           }}>
-            <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 20 }}>
-              <Button title="Prendre une photo" onPress={takePicture} />
-            </View>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-              <Button title="Zoom -" onPress={handleZoomOut} />
-              <Button title="Flip" onPress={toggleCameraType}></Button> 
-              <Button title="Zoom +" onPress={handleZoomIn} />
-            </View>
+          <View style={{ flex: 1, justifyContent: 'flex-end', marginBottom: 20 }}>
+          <TouchableOpacity style={styles.flashButton} onPress={toggleFlash}>
+            <Text style={styles.buttonText}><MaterialIcons name={flashMode === Camera.Constants.FlashMode.off ? "flash-off" : "flash-on"} size={45} color="white" /></Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.cameraButtons} onPress={takePicture}>
+            <Text style={styles.buttonText}><MaterialIcons name="radio-button-unchecked" size={100} color="white"></MaterialIcons></Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.flipCameraButton} onPress={toggleCameraType}>
+            <Text style={styles.buttonText}><MaterialIcons name="cached" size={50} color="white"></MaterialIcons></Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.zoomInButton} onPress={handleZoomIn}>
+            <Text style={styles.buttonText}><MaterialIcons name="zoom-in" size={60} color="white"></MaterialIcons></Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.zoomOutButton} onPress={handleZoomOut}>
+            <Text style={styles.buttonText}><MaterialIcons name="zoom-out" size={60} color="white"></MaterialIcons></Text>
+          </TouchableOpacity>
+          </View>
         </View>
       </Camera>
     </View>
@@ -130,4 +140,56 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  cameraButtons:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 0,
+    marginBottom: 20
+  },
+  flipCameraButton:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 300,
+    right: 0,
+    marginBottom: 580,
+  },
+  zoomOutButton:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 300,
+    marginBottom: 20,
+  },
+  zoomInButton:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 300,
+    right: 0,
+    marginBottom: 20,
+  },
+  flashButton:{
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 20,
+    left: 0,
+    right: 300,
+    marginBottom: 580,
+  },
+
+  
 });
